5cc8e80e45f2f8c674e672cf72cfbe82f8bd7eb4
fernandoCabrera06 <fernando_cabrera2006@hotmail.com>
2022-08-01 00:56:15 -0300

    cambios en numero de carrito y logica

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Reporte } from 'src/app/entidades/dto/Reporte';
 import { ProductosServicio } from 'src/app/servicios/productos.servicio';
 import { ArticuloManufacturado } from 'src/app/entidades/ArticuloManufacturado';
-import {Bebida} from 'src/app/entidades/dto/BebidaDto';
+import { Bebida } from 'src/app/entidades/dto/BebidaDto';
 import { DetalleProductoComponent } from '../detalleProducto/detalleProducto.component';
-import {detalleProductoManufactuardoServicio} from 'src/app/servicios/detalleProductoManufacturado.servicio';
-import{Carrito} from 'src/app/entidades/dto/CarritoDto';
-
+import { detalleProductoManufactuardoServicio } from 'src/app/servicios/detalleProductoManufacturado.servicio';
+import { Carrito } from 'src/app/entidades/dto/CarritoDto';
+import Swal from 'sweetalert2';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-catalogo',
@@ -23,13 +24,11 @@ export class CatalogoComponent implements OnInit {
     imagenArticuloManu: '',
     precioTotal: 0,
     stock: 0,
-    insumos:[],
-    idRubroGeneral:0
+    insumos: [],
+    idRubroGeneral: 0,
+  };
 
-  }
-
-  idRubroGeneral:number= 0;
-
+  idRubroGeneral: number = 0;
 
   articulos: ArticuloManufacturado[] = [];
 
@@ -42,18 +41,23 @@ export class CatalogoComponent implements OnInit {
   };
   bebidas: Bebida[] = [];
 
-  carrito: Carrito ={
-    id:0,
-    nombreProducto: "",
+  carrito: Carrito = {
+    id: 0,
+    nombreProducto: '',
     cantidad: 0,
     precioProducto: 0,
-    subTotal:0,
-    horasCocina:0
-  }
-  carritos : Carrito[]=[];
-  rol:string|null="";
+    subTotal: 0,
+    horasCocina: 0,
+  };
+  carritos: Carrito[] = [];
+  rol: string | null = '';
   loading = true;
-  constructor(private router: Router,private activeRoute:ActivatedRoute,private serv:ProductosServicio, private serArtManu:detalleProductoManufactuardoServicio ) {
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private serv: ProductosServicio,
+    private serArtManu: detalleProductoManufactuardoServicio
+  ) {
     this.rol = localStorage.getItem('rol');
   }
 
@@ -75,75 +79,97 @@ export class CatalogoComponent implements OnInit {
       }
       this.loading = false;
     });
-
-
   }
 
-  detalleProducto(i:number){
-     this.serArtManu.setProducto(this.articulos[i]);
-     this.router.navigate(['detalle']);
+  detalleProducto(i: number) {
+    this.serArtManu.setProducto(this.articulos[i]);
+    this.router.navigate(['detalle']);
   }
-  detalleBebida(i:number){
+  detalleBebida(i: number) {
     this.serArtManu.setBebida(this.bebidas[i]);
     this.router.navigate(['detalle']);
   }
 
-
-  agregar(i:number){
+  agregar(i: number) {
     if (this.hayStock(i)) {
       const dato = localStorage.getItem('carro');
-      alert("Producto agregado al carrito");
-      if(dato) this.carritos = JSON.parse(dato);
-      let nuevo ={
-        id:0,
-        nombreProducto:'',
-        cantidad:0,
-        precioProducto:0,
-        subTotal:0,
-        horasCocina:0
-      }
-       nuevo.id= this.articulos[i].idArticuloManufacturado;
-       nuevo.nombreProducto = this.articulos[i].denominacionArticuloManu;
-       nuevo.cantidad = 1;
-       nuevo.precioProducto = this.articulos[i].precioTotal;
-       nuevo.subTotal = this.articulos[i].precioTotal;
-       nuevo.horasCocina = this.articulos[i].tiempoEstimadoCocina;
-       this.carritos.push(nuevo);
-       localStorage.setItem('carro',JSON.stringify(this.carritos));
+      Swal.fire({
+        title: '¡Producto agregado al carrito!',
+        showDenyButton: false,
+        showCancelButton: false,
+        confirmButtonText: 'OK',
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          location.reload();
+        }
+      });
+
+      if (dato) this.carritos = JSON.parse(dato);
+      let nuevo = {
+        id: 0,
+        nombreProducto: '',
+        cantidad: 0,
+        precioProducto: 0,
+        subTotal: 0,
+        horasCocina: 0,
+      };
+      nuevo.id = this.articulos[i].idArticuloManufacturado;
+      nuevo.nombreProducto = this.articulos[i].denominacionArticuloManu;
+      nuevo.cantidad = 1;
+      nuevo.precioProducto = this.articulos[i].precioTotal;
+      nuevo.subTotal = this.articulos[i].precioTotal;
+      nuevo.horasCocina = this.articulos[i].tiempoEstimadoCocina;
+      this.carritos.push(nuevo);
+      localStorage.setItem('carro', JSON.stringify(this.carritos));
     }
   }
-  agregarBebida(i:number){
+  agregarBebida(i: number) {
     if (this.hayStock(i)) {
       const dato = localStorage.getItem('carro');
-      alert("Bebida agregada al carrito");
-      if(dato) this.carritos = JSON.parse(dato);
-      let nuevo ={
-        id:0,
-        nombreProducto:'',
-        cantidad:0,
-        precioProducto:0,
-        subTotal:0,
-        horasCocina:0
-      }
-       nuevo.id = this.bebidas[i].idBebida;
-       nuevo.nombreProducto = this.bebidas[i].nombreBebida;
-       nuevo.cantidad = 1;
-       nuevo.precioProducto = this.bebidas[i].precioTotal;
-       nuevo.subTotal = this.bebidas[i].precioTotal;
-       this.carritos.push(nuevo);
-       localStorage.setItem('carro',JSON.stringify(this.carritos));
+      Swal.fire({
+        title: '¡Bebida agregada al carrito!',
+        showDenyButton: false,
+        showCancelButton: false,
+        confirmButtonText: 'OK',
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          location.reload();
+        }
+      });
+      if (dato) this.carritos = JSON.parse(dato);
+      let nuevo = {
+        id: 0,
+        nombreProducto: '',
+        cantidad: 0,
+        precioProducto: 0,
+        subTotal: 0,
+        horasCocina: 0,
+      };
+      nuevo.id = this.bebidas[i].idBebida;
+      nuevo.nombreProducto = this.bebidas[i].nombreBebida;
+      nuevo.cantidad = 1;
+      nuevo.precioProducto = this.bebidas[i].precioTotal;
+      nuevo.subTotal = this.bebidas[i].precioTotal;
+      this.carritos.push(nuevo);
+      localStorage.setItem('carro', JSON.stringify(this.carritos));
     }
   }
 
-  hayStock(i:number){
+  avisoLogueo() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: '¡Ingrese con su usuario para comprar!',
+    });
+  }
+  hayStock(i: number) {
     let producto = this.articulos[i];
-    if(producto.stock>0){
+    if (producto.stock > 0) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-
-
-
-  }
+}
