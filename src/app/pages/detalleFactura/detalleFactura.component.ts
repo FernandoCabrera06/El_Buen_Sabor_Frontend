e97b679007543eca1bd46cf163ab64f1fda98744
fernandoCabrera06 <fernando_cabrera2006@hotmail.com>
2022-11-26 12:09:28 -0300

    fix domicilio factura - front

--- src/app/pages/detalleFactura/detalleFactura.component.ts
+++ src/app/pages/detalleFactura/detalleFactura.component.ts
@@ -10,7 +10,8 @@ import { Injectable } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import pdfMake from 'pdfmake/build/pdfMake';
-import pdfFonts from 'pdfmake/build/vfs_fonts'
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { Domicilio } from 'src/app/entidades/Domicilio';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-detalleFactura',
@@ -37,7 +38,7 @@ export class DetalleFacturaComponent implements OnInit {
     usuario: '',
     telefono: 0,
     rol: '',
-    domicilios: [],
+    domicilios: [new Domicilio()],
     bajaUsuario: false,
   };
 
@@ -58,11 +59,12 @@ export class DetalleFacturaComponent implements OnInit {
     });
   }
   ngOnInit(): void {
-    this.servFactura.getFacturaEnBaseDatosXId(this.factura.idFactura).subscribe(
+    this.servFactura
+      .getFacturaEnBaseDatosXId(this.factura.idFactura)
+      .subscribe(
         (facturaEncontrada: any) =>
           (this.factura = facturaEncontrada as Factura)
       );
-
 
     this.servUsuario
       .getUsuarioXnombreUsuarioFromDataBase(this.usuarioRol)
@@ -81,26 +83,36 @@ export class DetalleFacturaComponent implements OnInit {
     }
     const pdf = pdfMake.createPdf(pdfDefinition);
     pdf.open();*/
-    const DATA:any = document.getElementById('htmlData');
+    const DATA: any = document.getElementById('htmlData');
     const doc = new jsPDF('p', 'pt', 'a4');
     const options = {
       background: 'white',
-      scale: 3
+      scale: 3,
     };
-    html2canvas(DATA, options).then((canvas) => {
+    html2canvas(DATA, options)
+      .then((canvas) => {
+        const img = canvas.toDataURL('image/PNG');
 
-      const img = canvas.toDataURL('image/PNG');
-
-      // Add image Canvas to PDF
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_factura_El_Buen_Sabor.pdf`);
-    });
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(
+          img,
+          'PNG',
+          bufferX,
+          bufferY,
+          pdfWidth,
+          pdfHeight,
+          undefined,
+          'FAST'
+        );
+        return doc;
+      })
+      .then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_factura_El_Buen_Sabor.pdf`);
+      });
   }
 }
